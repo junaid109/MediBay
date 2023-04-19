c0340d178b8b61d747da1b70270796cf0a1bb5f9
Junaid <junmalik109@gmail.com>
2023-04-19 21:40:45 +0100

    Create Patient.cs

---
+++ MediBaseApp.Core/Models/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediBaseApp.Core.Models
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string BloodGroup { get; set; }
+        public string Allergies { get; set; }
+        public string MedicalHistory { get; set; }
+        public string Medications { get; set; }
+        public string Notes { get; set; }
+        
+      
+    }
+
+}
