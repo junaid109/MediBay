08dcecfca176cbe910417aea334812d7708dfd01
Junaid <junmalik109@gmail.com>
2023-04-19 21:40:37 +0100

    Create Appointment.cs

---
+++ MediBaseApp.Core/Models/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediBaseApp.Core
+{
+    public class Appointment
+    {
+        public string PatientName { get; set; }
+        public string DoctorName { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Reason { get; set; }
+        public string Notes { get; set; }
+    }
+}
