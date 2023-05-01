f3c089c894b8006c20851174bf226e8dd8ab1e65
Junaid <junmalik109@gmail.com>
2023-05-01 16:44:38 +0100

    Create IPatientDataService.cs

---
+++ MediBaseApp.Core/Services/IPatientDataService.cs
@@ -0,0 +1,18 @@
+﻿using MediBaseApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediBaseApp.Core.Services
+{
+    public interface IPatientDataService
+    {
+        Task<IList<Patient>> GetPatientsAsync();
+        
+       
+        
+        
+    }
+}
